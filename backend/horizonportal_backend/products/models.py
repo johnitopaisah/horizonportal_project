10ad138e85fa005f5eaac1397ad584f881b6e756
JohnItopaIsah <johnitopaisah@gmail.com>
2024-09-14 16:10:35 +0200

    update the business model

--- backend/horizonportal_backend/products/models.py
+++ backend/horizonportal_backend/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from business.models import BusinessProfile
 
-# Create your models here.
+
+class Product(models.Model):
+    business = models.ForeignKey(BusinessProfile, on_delete=models.CASCADE, related_name='product')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
