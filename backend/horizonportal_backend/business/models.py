f8fc9678790064c723979c5563bfae38ea39c93e
JohnItopaIsah <johnitopaisah@gmail.com>
2024-09-14 16:01:13 +0200

    update the business model

--- backend/horizonportal_backend/business/models.py
+++ backend/horizonportal_backend/business/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model
+
+class BusinessProfile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    business_name = models.CharField(max_length=255)
+    description = models.TextField()
+    website = models.URLField(blank=True, null=True)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(20)
+    logo = models.ImageField(upload_to='business_logo/', blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.business_name
+
