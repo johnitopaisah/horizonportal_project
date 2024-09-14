155d581d8b518c4a3b02b5bf34393014ee74e7e8
JohnItopaIsah <johnitopaisah@gmail.com>
2024-09-15 01:19:51 +0200

    modified the business view models

--- backend/horizonportal_backend/products/views.py
+++ backend/horizonportal_backend/products/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.exceptions import PermissionDenied
+from .models import Product 
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductListCreateView(generics.ListCreateView):
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+    def perform_create(self, serializer):
+        business = self.request.user.businessprofile
+        serializer.save(business=business)
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        if self.request.user.businessprofile == self.get_object().business:
+            serializer.save()
+        else:
+            raise PermissionDenied("You do not have the permission to edit this business profiles.")
